c637c07f55bf6e611ae63d48901e459493482497
EmilianoRivera <jriveras2100@alumno.ipn.mx>
2026-02-18 19:40:01 -0600

    corrección de credenciales

--- database.py
+++ database.py
@@ -3,9 +3,9 @@ from psycopg2.extras import RealDictCursor
 
 def obtener_conexion():
     return psycopg2.connect(
-        dbname="nna", # El nombre que pusiste en tu script
-        user="postgres",           # Tu usuario de Postgres
-        password="4skay$@",    # ¡Pon tu contraseña real aquí!
+        dbname="nna", 
+        user="postgres",   
+        password="",    # ¡Pon tu contraseña real aquí!
         host="localhost",
         port="5432"
     )
