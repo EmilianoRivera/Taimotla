6c83b76417355f29b2e792a9765ba78b495ca907
EmilianoRivera <jriveras2100@alumno.ipn.mx>
2026-03-01 23:14:27 -0600

    actualizo credenciales para la conexion a la bd

--- models/database.py
+++ models/database.py
@@ -1,11 +1,12 @@
 import psycopg2
 from psycopg2.extras import RealDictCursor
-
+import os
+from dotenv import load_dotenv
 def obtener_conexion():
     return psycopg2.connect(
-        dbname="nna_v2", 
-        user="postgres",   
-        password="4skay$@",    # ¡Pon tu contraseña real aquí!
-        host="localhost",
+        dbname=os.getenv('DB_NAME'), 
+        user=os.getenv('DB_USER'),   
+        password=os.getenv('DB_PASSWORD'),
+        host=os.getenv('DB_HOST'),
         port="5432"
     )
