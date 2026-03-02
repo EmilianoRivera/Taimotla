531c27c76a018aca69a86eb210569171bbbafba6
EmilianoRivera <jriveras2100@alumno.ipn.mx>
2026-03-01 22:35:09 -0600

    Funcionalidad de actualizar datos, borrar datos, habilitar y desabilitar cuenta

--- models/director.py
+++ models/director.py
@@ -238,3 +238,162 @@ def consult_director(curp):
     finally:
         if cur: cur.close()
         if conn: conn.close()
+
+
+def delete_user(curp):
+    try:
+        conn = obtener_conexion()
+        with conn.cursor() as cur:
+            tablas_roles = ['psicologo', 'trabajadorsocial', 'abogado', 'medico']
+            
+            for tabla in tablas_roles:
+                cur.execute(f'DELETE FROM public.{tabla} WHERE "CURP" = %s', (curp,))
+
+            cur.execute('DELETE FROM public.persona WHERE "CURP" = %s', (curp,))
+            
+            conn.commit()
+            return True
+    except psycopg2.Error as e:
+        print(f"❌ Error al eliminar: {e}")
+        if conn:
+            conn.rollback()
+    finally:
+        if cur: cur.close()
+        if conn: conn.close()
+
+def desable_user(curp):
+    try:
+        conn = obtener_conexion()
+        with conn.cursor() as cur:
+            tablas_roles = [ 'psicologo', 'trabajadorsocial', 'abogado', 'medico']
+            
+            for tabla in tablas_roles:
+                cur.execute(f'UPDATE public.{tabla} SET estado = %s WHERE "CURP" = %s', ("Inactivo",curp,))
+
+            
+            conn.commit()
+            return True
+    except psycopg2.Error as e:
+        print(f"❌ Error al inhabilitar al usuario: {e}")
+        if conn:
+            conn.rollback()
+    finally:
+        if cur: cur.close()
+        if conn: conn.close()
+
+
+def consult_unable_users():
+    try:
+        conn = obtener_conexion()
+        with conn.cursor() as cur:
+            query = """
+                SELECT p.p_nombre || ' ' || p.p_apellido, a."CURP", 'Abogado', a.estado 
+                FROM abogado a JOIN persona p ON a."CURP" = p."CURP" WHERE a.estado = 'Inactivo'
+                UNION
+                SELECT p.p_nombre || ' ' || p.p_apellido, m."CURP", 'Médico', m.estado 
+                FROM medico m JOIN persona p ON m."CURP" = p."CURP" WHERE m.estado = 'Inactivo'
+                UNION
+                SELECT p.p_nombre || ' ' || p.p_apellido, ts."CURP", 'TrabajadorSocial', ts.estado
+                FROM  trabajadorsocial ts JOIN  persona p ON ts."CURP" = p."CURP" WHERE ts.estado = 'Inactivo'
+                UNION
+                SELECT p.p_nombre || ' ' || p.p_apellido, ps."CURP", 'Psicólogo', ps.estado 
+                FROM psicologo ps JOIN persona p ON ps."CURP" = p."CURP" WHERE ps.estado = 'Inactivo';
+            """
+            cur.execute(query)
+            return cur.fetchall()
+        
+    except psycopg2.Error as e:
+        print(f"❌ Error al inhabilitar al usuario: {e}")
+        if conn:
+            conn.rollback()
+    finally:
+        if cur: cur.close()
+        if conn: conn.close()
+
+
+def able_user(curp):
+    try:
+        conn = obtener_conexion()
+        with conn.cursor() as cur:
+            tablas_roles = [ 'psicologo', 'trabajadorsocial', 'abogado', 'medico']
+            
+            for tabla in tablas_roles:
+                cur.execute(f'UPDATE public.{tabla} SET estado = %s WHERE "CURP" = %s', ("Activo",curp,))
+
+            
+            conn.commit()
+            return True
+    except psycopg2.Error as e:
+        print(f"❌ Error al habilitar al usuario: {e}")
+        if conn:
+            conn.rollback()
+    finally:
+        if cur: cur.close()
+        if conn: conn.close()   
+
+
+def search_one_user(curp):
+    try:
+        conn = obtener_conexion()
+        with conn.cursor() as cur:
+            tablas_roles = [ 'psicologo', 'trabajadorsocial', 'abogado', 'medico']
+            for tabla in tablas_roles:
+                query_get_user = f"""
+                SELECT 
+                    a."CURP" as curp,
+                    p.p_nombre || ' ' || p.p_apellido as nombre_completo, 
+                    p.telefono, 
+                    p.calle,
+                    p.correo
+
+                FROM public.{tabla} a 
+                JOIN public.persona p ON a."CURP" = p."CURP" 
+                WHERE p."CURP" = %s
+                """
+                res = cur.execute(query_get_user, (curp,))
+                res = cur.fetchone()
+                if res:
+                    return {
+                        'curp': res[0],
+                        'nombre': res[1],
+                        'telefono': res[2],
+                        'calle': res[3],
+                        'correo': res[4],
+                        'rol': tabla
+                    }
+            
+            return None
+            
+    except psycopg2.Error as e:
+        print(f"❌ Error al habilitar al usuario: {e}")
+        if conn:
+            conn.rollback()
+    finally:
+        if cur: cur.close()
+        if conn: conn.close()  
+
+
+def update_user(curp, data):
+    try:
+        conn = obtener_conexion()
+        with conn.cursor() as cur:
+            query = """
+                UPDATE public.persona 
+                SET correo = %s, telefono = %s, calle = %s 
+                WHERE "CURP" = %s
+            """
+            cur.execute(query, (
+                data.get('correo'), 
+                data.get('telefono'), 
+                data.get('calle'), 
+                curp
+            ))
+            conn.commit()
+            return True
+    except psycopg2.Error as e:
+        print(f"❌ Error al actualizar al usuario: {e}")
+        if conn:
+            conn.rollback()
+    finally:
+        if cur: cur.close()
+        if conn: conn.close()  
--- routes/director.py
+++ routes/director.py
@@ -1,11 +1,11 @@
 from flask import Blueprint, render_template, session, redirect, url_for, flash, request
-from models.director import user_register, consult_lawyers, consult_social, consult_medic, consult_psico, consult_director
+from models.director import user_register, consult_lawyers, consult_social, consult_medic, consult_psico, consult_director, delete_user, desable_user, consult_unable_users, able_user, search_one_user, update_user
 
 
 
 bp_director = Blueprint('director', __name__, url_prefix='/director')
 
-@bp_director.route('/dashboard')
+@bp_director.route('/dashboard', methods=['GET'])
 def dashboard():
     if session.get('rol') != 'director':
         flash("Acceso restringido a Directivos.", "error")
@@ -15,9 +15,13 @@ def dashboard():
         data_social =consult_social()
         data_medics = consult_medic()
         data_psico = consult_psico()
-
+        data_users_unable = consult_unable_users()
         print(data_medics)
-    return render_template('director/dashboard.html', nombre=session['nombre'], users_lawyers=data_lawyers, users_social = data_social, users_psico = data_psico, users_medics = data_medics)
+        print(data_psico)
+        print(data_social)
+        print(data_lawyers)
+
+    return render_template('director/dashboard.html', nombre=session['nombre'], users_lawyers=data_lawyers, users_social = data_social, users_psico = data_psico, users_medics = data_medics, users_unable = data_users_unable)
 
 @bp_director.route('/logout')
 def logout():
@@ -35,16 +39,49 @@ def cuenta():
         data_director = consult_director(str(curp))
         print(data_director)
     return render_template('director/cuenta.html', nombre=session['nombre'], user_director = data_director)
-    
+
+@bp_director.route('/editar/<curp>', methods=['GET', 'POST'])
+def editar(curp):
+    if session.get('rol') != 'director':
+        return redirect(url_for("main.index"))
+    user_found = None
+    if request.method == "GET":
+        print(curp)
+        user_found = search_one_user(curp)
+    return render_template('director/editar.html', user= user_found)
+
+@bp_director.route('/actualizar/<curp>', methods=['GET', 'POST'])
+def actualizar(curp):
+    if session.get('rol') != 'director':
+        return redirect(url_for("main.index"))
+    form_data = {
+        'correo': request.form.get('correo'),
+        'telefono': request.form.get('telefono'),
+        'calle': request.form.get('calle')
+    }
+
+    # 2. Llamar a la función de actualización
+    if update_user(curp, form_data):
+        flash("¡Datos actualizados correctamente!", "success")
+    else:
+        flash("Hubo un error al intentar actualizar.", "error")
+
+    # 3. Redirigir al dashboard para ver los cambios
+    return redirect(url_for('director.dashboard'))
 
 
-@bp_director.route('/editar')
-def editar():
-    return render_template('director/editar.html')
 
-@bp_director.route('/eliminar')
-def eliminar():
-    return render_template('director/eliminar.html')
+
+
+@bp_director.route('/eliminar/<curp>', methods=["POST"])
+def eliminar(curp):
+    if session.get('rol') != 'director':
+        flash("Acceso restringido a Directivos.", "error")
+        return redirect(url_for('main.login'))
+    if request.method == 'POST':
+   
+        eliminar = delete_user(curp)
+    return redirect(url_for("director.dashboard"))
 
 @bp_director.route('/registrar', methods=['GET','POST'])
 def registrar():
@@ -87,3 +124,27 @@ def registrar():
 
         return redirect(url_for("director.dashboard"))
     return render_template('director/registrar.html')
+
+
+@bp_director.route('/desable/<curp>', methods=['POST'])
+def desable(curp):
+    if session.get('rol') != 'director':
+        flash("Acceso restringido a Directivos.", "error")
+        return redirect(url_for("main.index"))
+    
+    if request.method == "POST":
+        state = desable_user(curp)
+        print(state)
+    return redirect(url_for("director.dashboard"))
+
+
+@bp_director.route('/able/<curp>', methods=['POST'])
+def able(curp):
+    if session.get('rol') != 'director':
+        flash("Acceso restringido a Directivos.", "error")
+        return redirect(url_for("main.index"))
+    
+    if request.method == "POST":
+        state = able_user(curp)
+        print(state)
+    return redirect(url_for("director.dashboard"))
